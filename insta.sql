dca53613eeae91ea7499c4de7e3636d61d44401b
Meet Rathod <139968514+Meet890@users.noreply.github.com>
2025-11-17 19:24:23 +0530

    make relationships

--- insta.sql
+++ insta.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 17, 2025 at 01:15 PM
+-- Generation Time: Nov 17, 2025 at 01:53 PM
 -- Server version: 9.1.0
 -- PHP Version: 8.3.14
 
@@ -31,14 +31,14 @@ DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE IF NOT EXISTS `accounts` (
   `acc_id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
-  `acc_profile_photo` varchar(200) NOT NULL,
-  `post_id` varchar(200) NOT NULL,
-  `acc_bio` varchar(200) NOT NULL,
+  `acc_profile_photo` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `acc_bio` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `acc_username` varchar(200) NOT NULL,
   `acc_password` varchar(200) NOT NULL,
   PRIMARY KEY (`acc_id`),
-  UNIQUE KEY `acc_username` (`acc_username`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `acc_username` (`acc_username`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -49,10 +49,12 @@ CREATE TABLE IF NOT EXISTS `accounts` (
 DROP TABLE IF EXISTS `block`;
 CREATE TABLE IF NOT EXISTS `block` (
   `block_id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `block_to` longtext NOT NULL,
-  PRIMARY KEY (`block_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `acc_id` int DEFAULT NULL,
+  `block_to` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  PRIMARY KEY (`block_id`),
+  UNIQUE KEY `block_to` (`block_to`(250)) USING BTREE,
+  KEY `acc_id` (`acc_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -63,11 +65,12 @@ CREATE TABLE IF NOT EXISTS `block` (
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE IF NOT EXISTS `comment` (
   `comm_id` int NOT NULL AUTO_INCREMENT,
-  `post_id` int NOT NULL,
-  `comm_by` longtext NOT NULL,
+  `post_id` int DEFAULT NULL,
+  `comm_by` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   PRIMARY KEY (`comm_id`),
-  KEY `comm_by` (`comm_by`(250))
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `comm_by` (`comm_by`(250)),
+  KEY `post_id` (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -83,8 +86,9 @@ CREATE TABLE IF NOT EXISTS `followers` (
   `foll_following` longtext NOT NULL,
   PRIMARY KEY (`foll_id`),
   KEY `foll_followers` (`foll_followers`(250)),
-  KEY `foll_following` (`foll_following`(250))
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `foll_following` (`foll_following`(250)),
+  KEY `acc_id` (`acc_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -97,8 +101,10 @@ CREATE TABLE IF NOT EXISTS `likes` (
   `like_id` int NOT NULL,
   `post_id` int NOT NULL,
   `like_by` longtext NOT NULL,
-  UNIQUE KEY `like_by` (`like_by`(250)) USING BTREE
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`like_id`),
+  UNIQUE KEY `like_by` (`like_by`(250)) USING BTREE,
+  KEY `post_id` (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -113,8 +119,10 @@ CREATE TABLE IF NOT EXISTS `pending_request` (
   `req_sender` int NOT NULL,
   `req_receiver` int NOT NULL,
   `req_status` varchar(1) NOT NULL,
-  PRIMARY KEY (`req_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`req_id`),
+  KEY `req_sender` (`req_sender`,`req_receiver`),
+  KEY `req_receiver` (`req_receiver`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -125,12 +133,15 @@ CREATE TABLE IF NOT EXISTS `pending_request` (
 DROP TABLE IF EXISTS `post`;
 CREATE TABLE IF NOT EXISTS `post` (
   `post_id` int NOT NULL AUTO_INCREMENT,
-  `post_type` varchar(200) NOT NULL,
-  `post_location` varchar(200) NOT NULL,
-  `like_id` int NOT NULL,
-  `comment_id` int NOT NULL,
-  PRIMARY KEY (`post_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `acc_id` int NOT NULL,
+  `post_type` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `post_location` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `like_id` int DEFAULT NULL,
+  `comment_id` int DEFAULT NULL,
+  PRIMARY KEY (`post_id`),
+  KEY `like_id` (`like_id`,`comment_id`),
+  KEY `acc_id` (`acc_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -151,7 +162,54 @@ CREATE TABLE IF NOT EXISTS `user_details` (
   UNIQUE KEY `user_email` (`user_email`),
   UNIQUE KEY `user_ph` (`user_ph`),
   KEY `Index_user_name` (`user_name`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD CONSTRAINT `accounts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_details` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `block`
+--
+ALTER TABLE `block`
+  ADD CONSTRAINT `block_ibfk_1` FOREIGN KEY (`acc_id`) REFERENCES `accounts` (`acc_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `followers`
+--
+ALTER TABLE `followers`
+  ADD CONSTRAINT `followers_ibfk_1` FOREIGN KEY (`acc_id`) REFERENCES `accounts` (`acc_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `pending_request`
+--
+ALTER TABLE `pending_request`
+  ADD CONSTRAINT `pending_request_ibfk_1` FOREIGN KEY (`req_sender`) REFERENCES `accounts` (`acc_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `pending_request_ibfk_2` FOREIGN KEY (`req_receiver`) REFERENCES `accounts` (`acc_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`acc_id`) REFERENCES `accounts` (`acc_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
