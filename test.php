c17395afc5802b34d9a4cef6e3fdec9748097013
Meet Rathod <139968514+Meet890@users.noreply.github.com>
2025-11-18 19:52:26 +0530

    end day-1 commit

--- test.php
+++ test.php
@@ -1,26 +1,26 @@
- <!-- if ($file_type == "image") {
-            echo '
-                <img src="'.$file_path.'" alt="post-image" class="gallery-img">
-            ';
-        }
-///////////////////////////////////////
-  <div id="myModal" class="modal" onclick="closeModal()">
-            
-            <img class="h-auto modal-content "  id="modalImg">
-        </div>
-//////////////////////////////////////
-<script>
-        function openModal(imageSrc) {
-        var modal = document.getElementById("myModal");
-        var modalImg = document.getElementById("modalImg");
-        modal.style.display = "block";
-        modalImg.src = imageSrc;
-        }
+Dummy Instagram (new mini project)
+-signup user
+-login user
+-create account
+-login account
+-build dummy UI with reference
+-profile page
+-home page
+-add posts *video or reel we can't uplode 
+-show posts in profile
+-edit profile details & private details
+-build actractive UI with online AI reference
 
-        function closeModal() {
-         var modal = document.getElementById("myModal");
-        modal.style.display = "none";
-        }
-    </script>
-
-         -->
+Pendding
+-all page check session session for redirect
+-show other persons
+-delete own post
+-follow other persons
+-see there posts , like it , comment it
+-add story's
+-view other persons story's
+-show profile photo in login,home,profile pages
+-dynamic followers,following,posts in profile page
+-search persons
+-view recommended posts 
+-add message opetion *if it is possible
--- user/logged/upload_post.php
+++ user/logged/upload_post.php
@@ -4,28 +4,28 @@ include "../conn.php";
 
 // Example logged-in user_id
 $user_id = $_SESSION['user_id'];
-
+echo'1<br>';
 if (isset($_POST['upload_post'])) {
-
+    echo'5<br>';
     $caption = $_POST['post_caption'];
     $file_name = $_FILES['post_file']['name'];
     $file_tmp = $_FILES['post_file']['tmp_name'];
-
+echo'1<br>';
     // File extension
     $ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
-
+echo'2<br>';
     // Validate extension
     $allowed = ['jpg','jpeg','png','mp4'];
     if (!in_array($ext, $allowed)) {
         die("Invalid file type!");
     }
-
+echo'3<br>';
     // Detect file type
     $file_type = in_array($ext, ['jpg','jpeg','png']) ? 'image' : 'video';
 
     // UNIQUE FILE NAME
     $unique_name = "post_" . uniqid("", true) . "_" . bin2hex(random_bytes(5)) . "." . $ext;
-
+echo'4<br>';
     // Local path
     $upload_path = "uploads/" . $unique_name;
 
