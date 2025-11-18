8766d2d2ebb203db47e4e794dc5e46c94a10cac9
Krupal Mehta <krupalmehta8@gmail.com>
2025-11-18 15:25:47 +0530

    commit done

---
+++ user/login_backend.php
@@ -0,0 +1,19 @@
+<?php
+require 'conn.php';
+
+if(isset($_POST['login_btn'])){
+    $user_email= $_POST['user_email'];
+    $user_password= $_POST['user_password'];
+    $sql = "select * from user_details where user_email = '$user_email' and user_password = '$user_password'";
+    $res = $conn->query($sql);
+
+    if($res -> num_rows > 0){
+        $_SESSION['user_email'] = $user_email;
+        header('Location: Home.php');
+        exit();
+}else {
+    header('Location: signup.php');
+}
+$conn->close();
+}
+?>
