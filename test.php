12467ed04e881672ab63eabd542e52d01947172f
Meet Rathod <139968514+Meet890@users.noreply.github.com>
2025-11-18 18:30:34 +0530

    add test file

---
+++ test.php
@@ -0,0 +1,26 @@
+ <!-- if ($file_type == "image") {
+            echo '
+                <img src="'.$file_path.'" alt="post-image" class="gallery-img">
+            ';
+        }
+///////////////////////////////////////
+  <div id="myModal" class="modal" onclick="closeModal()">
+            
+            <img class="h-auto modal-content "  id="modalImg">
+        </div>
+//////////////////////////////////////
+<script>
+        function openModal(imageSrc) {
+        var modal = document.getElementById("myModal");
+        var modalImg = document.getElementById("modalImg");
+        modal.style.display = "block";
+        modalImg.src = imageSrc;
+        }
+
+        function closeModal() {
+         var modal = document.getElementById("myModal");
+        modal.style.display = "none";
+        }
+    </script>
+
+         -->
